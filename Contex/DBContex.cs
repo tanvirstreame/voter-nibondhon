79976b8bbb0b63d4e4b8faea823008ffa7e7f1d7
Tanvir Islam streame <streametanvir@gmail.com>
2020-01-13 02:45:14 +0600

    Added registration page and sceenshots

--- Contex/DBContex.cs
+++ Contex/DBContex.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 // using Projectile.Models
 
-namespace projectile.Contex {
-    public class BloggingContext : DbContext
+namespace Projectile.Contex {
+    public class DBContex : DbContext
         {
             public DbSet<Person> Person { get; set; }
             protected override void OnConfiguring(DbContextOptionsBuilder options)
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Projectile.Models;
+using Projectile.Contex;
 
 namespace Projectile.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private DBContex _db = new DBContex();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int nid)
+        {
+            if(nid > 0) {
+                var data = _db.Person.Where(x => x.Id == nid).FirstOrDefault();
+                ViewData["SearchedData"] = data;
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(Person person)
+        {
+            _db.Person.Add(person);
+            int flag = _db.SaveChanges();
+            ViewData["nid"] = person.Id.ToString().PadLeft(8, '0');;
+            ViewData["Message"] = flag == 1 ? "Data inserted successfully" : "Failed";
+
             return View();
         }
 
